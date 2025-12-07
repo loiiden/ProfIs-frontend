404a64efc3f1e69e93abc9e70c9185429786dd06
anton <antonpaulr@protonmail.com>
2025-12-07 11:58:32 +0100

    added structure for arbeit erfassen page

---
+++ src/lib/components/erfassen-noten.svelte
@@ -0,0 +1,17 @@
+<div class="erfassen-noten-container">
+</div>
+
+<style lang="scss">
+.erfassen-noten-container {
+    grid-column-start: 1;
+    grid-column-end: 8;
+    grid-row-start: 9;
+    grid-row-end: 16;
+
+    background-color: #FFFFFF;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    padding: 12px;
+    border-radius: 10px;
+}
+
+</style>
---
+++ src/lib/components/preview-overview-arbeit.svelte
@@ -0,0 +1,17 @@
+<div class="preview-overview-arbeit-container">
+</div>
+
+<style lang="scss">
+.preview-overview-arbeit-container {
+    grid-column-start: 1;
+    grid-column-end: 8;
+    grid-row-start: 2;
+    grid-row-end: 9;
+
+    background-color: #FFFFFF;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    padding: 12px;
+    border-radius: 10px;
+}
+</style>
+
