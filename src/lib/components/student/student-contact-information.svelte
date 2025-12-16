3c367ebc7f5249266c1a653ce75a43f262eaa796
Anton <antonpaulr@protonmail.com>
2025-12-16 11:50:56 +0100

    added structure for referent, student erfassen

---
+++ src/lib/components/student/student-contact-information.svelte
@@ -0,0 +1,17 @@
+<div class="student-contact-information-container">
+</div>
+
+<style lang="scss">
+.student-contact-information-container {
+    grid-column-start: 7;
+    grid-column-end: 12;
+    grid-row-start: 2;
+    grid-row-end: 14;
+
+    background-color: #FFFFFF;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    padding: 12px;
+    border-radius: 10px;
+}
+
+</style>
---
+++ src/lib/components/student/student-study-information.svelte
@@ -0,0 +1,17 @@
+<div class="student-study-information-container">
+</div>
+
+<style lang="scss">
+.student-study-information-container {
+    grid-column-start: 1;
+    grid-column-end: 7;
+    grid-row-start: 8;
+    grid-row-end: 14;
+
+    background-color: #FFFFFF;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    padding: 12px;
+    border-radius: 10px;
+}
+
+</style>
